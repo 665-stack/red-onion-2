9b8f58339f4322498bac9ab65d112221579129c6
MahmodulShahin <mahmodulshahin665@gmail.com>
2022-11-23 03:36:01 +0600

    add header and header style

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './Components/Pages/Home/Home/Home';
 import Login from './Components/Pages/Login/Login/Login';
+import Signup from './Components/Pages/Login/SignUp/Signup';
+import ShoppingCart from './Components/Pages/ShoppingCart/ShoppingCart';
 import Header from './Components/Shared/Header/Header';
 import NotFound from './Components/Shared/NotFound/NotFound';
 
@@ -11,7 +13,10 @@ function App() {
       <Header></Header>
       <Routes>
         <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/cart" element={<ShoppingCart></ShoppingCart>}></Route>
         <Route path="/login" element={<Login></Login>}></Route>
+        <Route path="/signup" element={<Signup></Signup>}></Route>
+
         <Route path="*" element={<NotFound></NotFound>}></Route>
       </Routes>
     </>
--- src/Components/Shared/Header/Header.js
+++ src/Components/Shared/Header/Header.js
@@ -4,13 +4,16 @@ import logo2 from '../../../images/logo2.png'
 import './Header.css'
 const Header = () => {
     return (
-        <div className='flex '>
-            <img className='logoImg' src={logo2} alt="red onion logo" />
-            <div>
-                <Link to='/card'>Card</Link>
+        <div className='flex justify-between items-center	bg-slate-50 px-4 sm:px-12 py-4'>
+
+            <Link to='/'><img className='logoImg' src={logo2} alt="red onion logo" /></Link>
+
+            <div className='header-link '>
+                <Link to='/cart'>Cart</Link>
                 <Link to='/login'>Login</Link>
-                <Link to='/signup'>Sign Up</Link>
+                <Link className='bg-rose-600 px-6 py-2 text-white rounded-full' to='/signup'>Sign Up</Link>
             </div>
+
         </div>
     );
 };
