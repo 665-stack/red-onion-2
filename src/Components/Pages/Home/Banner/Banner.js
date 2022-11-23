eb6132ea86e0184c9b82c51a5d8989eef822f086
MahmodulShahin <mahmodulshahin665@gmail.com>
2022-11-23 23:22:18 +0600

    fix input style in banner component

--- src/Components/Pages/Home/Banner/Banner.js
+++ src/Components/Pages/Home/Banner/Banner.js
@@ -3,11 +3,11 @@ import './Banner.css'
 const Banner = () => {
     return (
         <div className='banner'>
-            <div className=' px-1 sm:px-5'>
-                <h1 className='font-semibold text-3xl md:text-5xl'>Best food waiting for your belly</h1>
+            <div className='banner-div px-1 sm:px-5'>
+                <h1 className='font-semibold  text-3xl sm:text-4xl md:text-5xl'>Best food waiting for your belly</h1>
                 <br />
-                <input className='font-semibold	bg-white px-10 py-1 rounded-full' type="text" placeholder='search something' />
-                <button className=' bg-rose-600 px-6 py-2 text-white rounded-full font-semibold	'>Search</button>
+                <input className='font-semibold	bg-white px-4 py-2  rounded-full' type="text" placeholder='search something' />
+                <button className=' bg-rose-600 mt-2 sm:mt-0 px-6 py-2 text-white rounded-full font-semibold	'>Search</button>
             </div>
         </div >
     );
--- src/Components/Shared/Header/Header.js
+++ src/Components/Shared/Header/Header.js
@@ -4,11 +4,12 @@ import logo2 from '../../../images/logo2.png'
 import './Header.css'
 const Header = () => {
     return (
-        <div className='flex justify-center sm:justify-between items-center	bg-white px-4 sm:px-12 py-3'>
+        <div className='flex justify-center sm:justify-between items-center	bg-white px-2 sm:px-12 py-3'>
 
             <Link to='/'><img className='logoImg hidden sm:block' src={logo2} alt="red onion logo" /></Link>
 
             <div className='header-link'>
+                <Link className='sm:hidden' to='/'>Home</Link>
                 <Link to='/cart'>Cart</Link>
                 <Link to='/login'>Login</Link>
                 <Link className='bg-rose-600 px-6 py-2 text-white rounded-full' to='/signup'>Sign Up</Link>
