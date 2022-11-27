22ed033042d2f2d39fb48a8bb7d96928c0914ece
MahmodulShahin <mahmodulshahin665@gmail.com>
2022-11-27 16:40:04 +0600

    add conditional signour button on header

--- src/Components/Shared/Header/Header.js
+++ src/Components/Shared/Header/Header.js
@@ -1,8 +1,17 @@
+import { signOut } from 'firebase/auth';
 import React from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
 import { Link } from 'react-router-dom';
+import auth from '../../../firebase.init';
 import logo2 from '../../../images/logo2.png'
 import './Header.css'
 const Header = () => {
+    const [user] = useAuthState(auth);
+
+    const handleSignOut = () => {
+        signOut(auth)
+    }
+
     return (
         <div className='flex justify-center sm:justify-between items-center	bg-white px-2 sm:px-12 py-3'>
 
@@ -11,8 +20,13 @@ const Header = () => {
             <div className='header-link'>
                 <Link className='sm:hidden' to='/'>Home</Link>
                 <Link to='/cart'>Cart</Link>
-                <Link to='/login'>Login</Link>
-                <Link className='bg-rose-600 px-6 py-2 text-white rounded-full' to='/signup'>Sign Up</Link>
+
+                {user ? <button onClick={handleSignOut} className="bg-rose-600 px-6 py-2 text-white rounded-full ml-3">Sign Out</button>
+                    : <>
+                        <Link to='/login'>Login</Link>
+                        <Link className='bg-rose-600 px-6 py-2 text-white rounded-full' to='/signup'>Sign Up</Link>
+                    </>
+                }
             </div>
 
         </div>
